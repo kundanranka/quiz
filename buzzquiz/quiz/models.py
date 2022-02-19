f1c4657fcfdafef1cde72bc5ebfd11caa0e845ba
Kundan <kunnuranka@gmail.com>
2022-02-20 00:41:40 +0530

    lazy commits reverted :P

--- buzzquiz/quiz/models.py
+++ buzzquiz/quiz/models.py
@@ -1,7 +1,4 @@
-from tkinter import CASCADE
-from click import option
 from django.db import models
-from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
